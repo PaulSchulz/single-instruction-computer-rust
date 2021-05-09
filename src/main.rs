e266a917c3ff62c3e5ad884a70624964cb4c8a26
Paul Schulz <paul@mawsonlakes.org>
2021-05-09 16:45:57 +0930

    main: Annotates example with Assembly
    Also, adds start of Assembly documentation (Assembler.org) prior to implementing
    a Assembler.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn introduction() {
 // See: https://stackoverflow.com/questions/24831828/how-do-i-pass-an-array-to-a-function-in-rust-and-change-its-content
 
 // FIXME: Check for negative 'diff' as this causes rust code to abort due to integer overflow
-fn process(memory: &mut [u16; 0xffff], pc: u16) -> u16 {
+fn process(memory: &mut [u16; 0x10000], pc: u16) -> u16 {
     let ptr_a: u16 = memory[pc as usize];
     let ptr_b: u16 = memory[pc as usize + 1];
     let ptr_c: u16 = memory[pc as usize + 2];
@@ -28,14 +28,24 @@ fn process(memory: &mut [u16; 0xffff], pc: u16) -> u16 {
             pc, ptr_a, a, ptr_b, b, ptr_c
         );
     }
-    let diff = b - a;
+
+    let diff: u16;
+    diff = b.wrapping_sub(a);
+    //    if b > a {
+    //    let diff = b - a;
+    //} else {
+    //
+    //}
 
     // Write result
     memory[ptr_b as usize] = diff;
-    if diff as isize > 0 {
-        ret_pc = pc + 3;
-    } else {
+    // Is the MSB clear (negative number)?
+    if (diff == 0) || (diff & 0b1000_0000) != 0 {
+        // Branch
         ret_pc = ptr_c;
+    } else {
+        // Next Instuction
+        ret_pc = pc + 3;
     }
 
     if DEBUG {
@@ -47,8 +57,8 @@ fn process(memory: &mut [u16; 0xffff], pc: u16) -> u16 {
     return ret_pc;
 }
 
-fn list_page(memory: [u16; 0xffff], pc: u16, page: u16) {
-    for i in (page..page + 10).step_by(3) {
+fn list_page(memory: [u16; 0x10000], pc: u16, page: u16) {
+    for i in (page..page + 21).step_by(3) {
         if (page <= pc) && (pc < page + 3) {
             print!("> ");
         } else {
@@ -69,23 +79,38 @@ fn list_page(memory: [u16; 0xffff], pc: u16, page: u16) {
 fn main() {
     // Processor data
     let mut pc: u16 = 0x0000;
-    let mut memory: [u16; 0xffff] = [0u16; 65535];
+    let mut memory: [u16; 0x10000] = [0u16; 0x10000];
 
-    println!("DEBUG: Clearing memeory to 0");
+    println!("DEBUG: Clearing memeory to 0x0");
     for i in 0..memory.len() {
         memory[i] = 0x0000;
     }
     introduction();
 
-    // Testing
+    // Testing - Iterating Loop
+    // L:0 1 ?
+    // A B L
+    // ; Data
+    // A:1 B:0 0
+
     memory[0x0000] = 0x0000;
     memory[0x0001] = 0x0001;
     memory[0x0002] = 0x0003;
 
+    memory[0x0003] = 0x0006;
+    memory[0x0004] = 0x0007;
+    memory[0x0005] = 0x0000;
+    // Data
+    memory[0x0006] = 0x0001;
+    memory[0x0007] = 0x0000;
+    memory[0x0008] = 0x0000;
+
     list_page(memory, pc, 0x0000);
 
-    pc = process(&mut memory, pc);
-    pc = process(&mut memory, pc);
+    // Processing loop
+    for _i in 0..6 {
+        pc = process(&mut memory, pc);
+    }
 
     list_page(memory, pc, 0x0000);
 }
